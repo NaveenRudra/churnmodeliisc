e2f52f86709f74d08f880468350db8152445cde9
naveen.rudra02@gmail.com <naveen.rudra02@gmail.com>
2022-07-05 13:00:19 +0530

    update

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -87,7 +87,6 @@ def train_and_evaluate(config_path):
        
         tracking_url_type_store = urlparse(mlflow.get_artifact_uri()).scheme
 
-   
         if tracking_url_type_store != "file":
             mlflow.sklearn.log_model(
                 model, 
